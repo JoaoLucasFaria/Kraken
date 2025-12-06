8458dabb772040e041a27efdd425a87b6bc9bffb
joaoVs17 <joaosimao051@gmail.com>
2025-12-06 19:11:19 -0300

    UPDATE SCRAPPER

--- src/scrapper.py
+++ src/scrapper.py
@@ -6,23 +6,53 @@ import os
 import csv
 import time
 import random
+from urls import urlsA, urlsB, urlsC
+import re
+
+def normalize_name(name: str) -> str:
+  name = name.strip()
+  name = re.sub(r"\(.*?\)", "", name).strip()
+  if "," in name:
+      parts = [p.strip() for p in name.split(",")]
+      name = parts[-1]  # última parte
+  name = re.sub(r"^Swarm of\s+", "", name, flags=re.I)
+  name = name.replace(" ", "_")
+  name = "_".join(word.capitalize() for word in name.split("_"))
+
+  return name
+
+def load_names(filepath: str):
+  normalized_names = set()
+
+  with open(filepath, "r", encoding="utf-8") as f:
+    for line in f:
+      line = line.strip()
+      if not line:
+          continue
+      name = normalize_name(line)
+      normalized_names.add(name)
+
+  name_list = list(normalized_names)
+  name_list.sort()
+  return name_list
+
 
 def log_error(result: dict):
-  with open("errors.txt", "a", encoding="utf-8") as f:
+  with open("errors2.txt", "a", encoding="utf-8") as f:
     f.write(
-      f"{result['url']}\n"
+      f"{result['url']} - {result['message']}\n"
     )
 
 def save_success_to_csv(monster_name: str, description: str):
-  file_exists = os.path.isfile("output.csv")
+  file_exists = os.path.isfile("output2.csv")
 
-  with open("output.csv", "a", encoding="utf-8", newline="") as csvfile:
-    writer = csv.writer(csvfile)
+  with open("output2.csv", "a", encoding="utf-8", newline="") as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
     if not file_exists:
       writer.writerow(["monster_name", "description"])
-
-    writer.writerow([monster_name, description])
+    cleaned_description = description.strip().replace('\n', ' ').replace('\r', ' ')
+    writer.writerow([monster_name, cleaned_description])
 
 def extract_description(url: str) -> dict[str, Any]:
     try:
@@ -83,17 +113,32 @@ def extract_description(url: str) -> dict[str, Any]:
 
 
 if __name__ == "__main__":
-    urls = [
-      "https://forgottenrealms.fandom.com/wiki/Kenkus",
-    ]
+  # urls = [
+  #   "https://forgottenrealms.fandom.com/wiki/Kenkus",
+  # ]
 
-    for url in urls:
-      print(f"\n--- URL: {url} ---")
-      time.sleep(random.uniform(1, 5))
-      res = extract_description(url)
-      
-      if (res.get("success") == False):
-        log_error(res)
-      else:
-        save_success_to_csv(res["monster_name"], res["description"])
+  file_path = "monster_names.txt"
+  name_list = load_names(file_path)
+  print(len(name_list))
+  print(name_list)
+  
+  current_initial = "A"
+ 
+  for name in name_list:
+    title_name= name.lower()
+    title_name = title_name[0].upper() + title_name[1:]
+    
+    if title_name[0] != current_initial:
+      current_initial = title_name[0]
+      random.uniform(10, 15)
+    
+    url = f"https://forgottenrealms.fandom.com/wiki/{title_name}"
+    print(f"\n--- URL: {url} ---")
+    time.sleep(random.uniform(1, 5))
+    res = extract_description(url)
+    
+    if (res.get("success") == False):
+      log_error(res)
+    else:
+      save_success_to_csv(res["monster_name"], res["description"])
         
